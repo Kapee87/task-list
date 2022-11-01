b50ef1bd6086a81d5a4daaffc7924af33073fd58
Kapee87 <kapserker@gmail.com>
2022-11-01 15:12:04 -0300

    Primer componente header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title:string = 'My Task List';
   constructor() { }
 
   ngOnInit(): void {
